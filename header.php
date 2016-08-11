e58983481c194c3d73d8f5980144789093c36b93
Gian Carlo Egamino <gnegamino@gmail.com>
2016-08-11 18:15:05 +0800

    Updated Title header

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>D`UNICO</title>
+		<title>Welcome to D'UNICO Studio</title>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 		<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
